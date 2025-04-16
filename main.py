cc5f1f7126fa1d9dbcda2b73ad46eb7cdd3c42ce
RamonMartinez01 <mafrtinezcjr1@gmail.com>
2025-04-16 13:45:29 -0700

    Actualiza el archivo main.py

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 
 from fastapi import FastAPI
+from fastapi import Depends, Query
+from sqlalchemy.orm import Session
+from app.database import SessionLocal
 
-# Initialize FastAPI app
+from fastapi.middleware.cors import CORSMiddleware
+#from app.controllers.tu_jemplo_controller import get_all_funciónEjemplo     #Aquí importarás las funciones desde tus controllers
+#from app.routes.tu_ejemplo_router import router as ejemplo_router           #Aquí importarás tus rutas desde tus archivos router
+
+# Inicializa FastAPI app
 app = FastAPI()
+
+# Configuración CORS 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite cualquier origen (en producción, especificar dominios permitidos)
+    allow_credentials=True,
+    allow_methods=["*"],   # Permite todos los métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],   # Permite todos los encabezados 
+)
+
+#
+#app.include_router(ejemplo_router)  #Incluye tus routers en este espacio
+#
 
 @app.get("/")
 def read_root():
-    return {"message": "Checka estooooo!"}
+    return {"message": "Bienvenido a tu nueva API!"}
